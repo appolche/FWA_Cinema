d241f9e167f3e556b8aab5d71193b085ab067f0f
dleaves <appol.che@gmail.com>
2022-11-26 17:59:52 +0300

    add UsersServlet

---
+++ src/main/java/edu/school21/cinema/servlets/UsersServlet.java
@@ -0,0 +1,15 @@
+package edu.school21.cinema.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/users")
+public class UsersServlet extends HttpServlet {
+//    private UsersService usersService;
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        ServletContext context = config.getServletContext();
+//        ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
+//        this.usersService = springContext.getBean(UsersService.class);
+//    }
+}
