4ac571f049437111a80dc28aa6f2094f25b94717
Elvek Goryaev <e.goryaev@digitalhorizon.ru>
2022-11-27 20:44:38 +0300

    Tomcat configuration. Fix bugs

--- src/main/java/edu/school21/cinema/servlets/UsersServlet.java
+++ src/main/java/edu/school21/cinema/servlets/UsersServlet.java
@@ -1,15 +1,20 @@
 package edu.school21.cinema.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/users")
+@WebServlet(value = "/users", name = "UsersServlet")
 public class UsersServlet extends HttpServlet {
-//    private UsersService usersService;
-//    @Override
-//    public void init(ServletConfig config) throws ServletException {
-//        ServletContext context = config.getServletContext();
-//        ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
-//        this.usersService = springContext.getBean(UsersService.class);
-//    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter w = resp.getWriter();
+        w.println("sobaka");
+        w.close();
+    }
 }
