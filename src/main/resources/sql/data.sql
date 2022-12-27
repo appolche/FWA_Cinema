3c9e48b6548addfedb639a66efd189032b8d51cc
dleaves <appol.che@gmail.com>
2022-12-27 21:39:01 +0300

    add readme and data.sql

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,8 @@
+insert into users (email, first_name, last_name, phone_number, password) VALUES
+                                            ('kot@gmeow.com', 'kot', 'koteev', '789', 'fghj'),
+                                            ('kit@gmeow.com', 'kit', 'kiteev', '789', 'fggsgg'),
+                                            ('kitechka@gmeow.com', 'kitechka', 'kitechkova', '324', 'dfgfg'),
+                                            ('meow@gmeow.com', 'meow', 'meowmeow', '41232314', 'uukiyu'),
+                                            ('mur@gmeow.com', 'mur', 'murmur', '5542542', 'qwrwewe');
+
+select * from users;
--- src/main/resources/sql/testData.sql
+++
@@ -1,6 +0,0 @@
-insert into users (first_name, last_name, phone_number, password) VALUES
-                                            ('kot', '1@1",''11111111', '789', 'fghj'),
-                                            ('kit', '11111111', '789', 'fghj'),
-                                            ('kitechka', '11111111', '789', 'fghj'),
-                                            ('meow', '11111111', '789', 'fghj'),
-                                            ('mur', '11111111', '789', 'fghj');
